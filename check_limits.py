a1616d2e0f0fc2a7fd09ad6e3bc9ab2c7fdffad2
shreyaslall <60237982+shreyaslall@users.noreply.github.com>
2022-09-09 12:35:16 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -27,5 +27,5 @@ def battery_is_ok(temp, soc, chrate):
   
  
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is False)
-  assert(battery_is_ok(50, 85, 0) is True)
+  assert(battery_is_ok(25, 70, 0.7) is True)
+  assert(battery_is_ok(50, 85, 0) is False)
